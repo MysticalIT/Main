b13e90a41cac5a8bb2650c3fadfc27af30c3f0b8
BuildTools <pvanhalm00@gmail.com>
2017-09-25 10:30:33 +0200

    Development Added

---
+++ public/Development/main.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Development - Main</title>
+</head>
+<body>
+<div class="container">
+    <div class="wrapper">
+        <ul>
+            <li><a href="projects.php">Projects</a></li>
+            <li><a href="memo.php">Memo's</a></li>
+            <li><a href="Logout.php">Logout</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
