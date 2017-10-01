bfe4612b9ad512eafcc5233bfe8fc9bd77d7b991
EXtermin <Damian.meijer@gmail.com>
2017-10-01 13:42:30 +0200

    Sales 50% done

---
+++ Barroc-IT/resources/views/404_page.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>you no work c:</title>
+</head>
+<body>
+<h1>UNDER CONTSTRUCTION C:</h1>
+
+
+
+
+<p>Have nyan cats c;</p>
+
+<iframe src="https://giphy.com/embed/sIIhZliB2McAo" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+<iframe src="https://giphy.com/embed/sIIhZliB2McAo" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+<iframe src="https://giphy.com/embed/sIIhZliB2McAo" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+<iframe src="https://giphy.com/embed/sIIhZliB2McAo" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+<!--<audio controls autoplay loop > <source src="NyanCatoriginal.wav"> </audio> -->
+
+</body>
+</html>
