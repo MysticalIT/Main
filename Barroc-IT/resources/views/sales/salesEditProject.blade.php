90accdb2d9612f12e740299a4ebb6a6aff67c8bc
Unknown <pvanhalm00@gmail.com>
2017-10-10 09:31:11 +0200

    Sales Fix

--- Barroc-IT/resources/views/sales/salesEditProject.blade.php
+++ Barroc-IT/resources/views/sales/salesEditProject.blade.php
@@ -13,7 +13,7 @@
 <body>
 
     <form action="/projects/{{$project->id}}" method="post">
-		{{csrf_field}}
+		{{csrf_field()}}
 		{{method_field('PUT')}}
         <label for="projectName">Project name</label>
         <input type="text" name="projectName" id="projectName" value="{{$project->name}}">
